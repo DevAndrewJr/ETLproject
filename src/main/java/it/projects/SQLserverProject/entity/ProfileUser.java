680ebc526ccfa52b6c736cd7011f67f02936c158
Andrea Giaimis <sviluppoweb74@gmail.com>
2024-04-29 10:55:45 +0200

    Creating ProfileUser in entity

---
+++ src/main/java/it/projects/SQLserverProject/entity/ProfileUser.java
@@ -0,0 +1,53 @@
+package it.projects.SQLserverProject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "userProfile")
+public class ProfileUser {
+
+    @Id
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="street")
+    private String street;
+
+    @Column(name="suite")
+    private String suite;
+
+    @Column(name="city")
+    private String city;
+
+    @Column(name="zipcode")
+    private String zipcode;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="website")
+    private String website;
+
+    @Column(name="lat")
+    private String lat;
+
+    @Column(name="lng")
+    private String lng;
+
+    @Column(name="companyName")
+    private String companyName;
+
+    @Column(name="companyCatchPhrase")
+    private String companyCatchPhrase;
+
+    @Column(name="companyBs")
+    private String companyBs;
+}
