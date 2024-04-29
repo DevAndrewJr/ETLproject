6cec15abf49f990c613881e5a1cd905099355339
Andrea Giaimis <sviluppoweb74@gmail.com>
2024-04-29 11:02:18 +0200

    Creating ProfileUserRepository

---
+++ src/main/java/it/projects/SQLserverProject/repository/ProfileUserRepository.java
@@ -0,0 +1,8 @@
+package it.projects.SQLserverProject.repository;
+
+import it.projects.SQLserverProject.entity.*;
+
+import org.springframework.data.jpa.repository.*;
+
+public interface ProfileUserRepository extends JpaRepository< ProfileUser, Integer> {
+}
