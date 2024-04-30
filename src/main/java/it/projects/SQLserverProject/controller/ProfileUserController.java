2b05bc7ee9a7e8d4a8b1fad7ff40cd963cca649c
Andrea Giaimis <sviluppoweb74@gmail.com>
2024-04-30 22:28:39 +0200

    Creating controller class ProfileUserController

---
+++ src/main/java/it/projects/SQLserverProject/controller/ProfileUserController.java
@@ -0,0 +1,29 @@
+package it.projects.SQLserverProject.controller;
+
+import it.projects.SQLserverProject.service.*;
+import it.projects.SQLserverProject.dto.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/project/api/json")
+public class ProfileUserController {
+
+    private final ProfileUserService profileUserService;
+
+    @Autowired
+    public ProfileUserController(ProfileUserService profileUserService) {
+        this.profileUserService = profileUserService;
+    }
+
+    @PostMapping(value = "/users/save", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<String> saveJsonUser() {
+        profileUserService.saveUsersToProfile();
+        return ResponseEntity.ok("Users saved to profile successfully");
+    }
+}
