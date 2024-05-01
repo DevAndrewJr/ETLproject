b3882722baef9d959cee6faaf197b79e7b464e52
Andrea Giaimis <sviluppoweb74@gmail.com>
2024-05-01 12:03:08 +0200

    Formatting and updating ProfileUserService and ETLprojectApplication

--- src/main/java/it/projects/SQLserverProject/ETLprojectApplication.java
+++ src/main/java/it/projects/SQLserverProject/ETLprojectApplication.java
@@ -16,49 +16,10 @@ import java.util.Map;
 @SpringBootApplication
 public class ETLprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ETLprojectApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ETLprojectApplication.class, args);
 
 
-		FetchJsonPlaceholderUserService fetchService = new FetchJsonPlaceholderUserService();
-
-		// Ottieni la lista degli utenti
-		List<JsonPlaceholderUser> userList = fetchService.fetchUserList();
-
-		// Stampa tutti i dati degli utenti
-		for (JsonPlaceholderUser user : userList) {
-			System.out.println("User Details:");
-			System.out.println("ID: " + user.getId());
-			System.out.println("Name: " + user.getName());
-			System.out.println("Username: " + user.getUsername());
-			System.out.println("Email: " + user.getEmail());
-			System.out.println("Phone: " + user.getPhone());
-			System.out.println("Website: " + user.getWebsite());
-
-			// Stampa l'indirizzo dell'utente
-			System.out.println("Address:");
-			System.out.println("Street: " + user.getAddress().getStreet());
-			System.out.println("Suite: " + user.getAddress().getSuite());
-			System.out.println("City: " + user.getAddress().getCity());
-			System.out.println("Zipcode: " + user.getAddress().getZipcode());
-
-			// Stampa le coordinate geografiche
-			System.out.println("Geo:");
-			System.out.println("Lat: " + user.getAddress().getGeo().getLat());
-			System.out.println("Lng: " + user.getAddress().getGeo().getLng());
-
-			// Stampa i dettagli della compagnia
-			System.out.println("Company:");
-			System.out.println("Name: " + user.getCompany().getName());
-			System.out.println("Catch Phrase: " + user.getCompany().getCatchPhrase());
-			System.out.println("Bs: " + user.getCompany().getBs());
-
-			System.out.println(); // Aggiungi una riga vuota per separare i dati degli utenti
-
-		}
-
-
-
-	}
-
+    }
 }
+
--- src/main/java/it/projects/SQLserverProject/service/ProfileUserService.java
+++ src/main/java/it/projects/SQLserverProject/service/ProfileUserService.java
@@ -24,7 +24,7 @@ public class ProfileUserService {
 
     public void saveUsersToProfile() {
         try {
-            // Ottieni la lista delle persone dalla chiamata API
+            // Get a people list of the API call
             List< Map< String, Object > > peopleList = fetchJsonPlaceholderUserService.getListPeople();
 
             // Itera attraverso ogni mappa di persona
@@ -36,7 +36,7 @@ public class ProfileUserService {
                 profileUser.setUsername((String) userMap.get("username"));
                 profileUser.setEmail((String) userMap.get("email"));
 
-                Map<String, Object> addressMap = (Map<String, Object>) userMap.get("address");
+                Map< String, Object > addressMap = (Map< String, Object >) userMap.get("address");
                 if (addressMap != null) {
                     profileUser.setStreet((String) addressMap.get("street"));
                     profileUser.setSuite((String) addressMap.get("suite"));
@@ -48,14 +48,14 @@ public class ProfileUserService {
                 profileUser.setWebsite((String) userMap.get("website"));
 
 
-                Map<String, Object> geoMap = (Map<String, Object>) userMap.get("geo");
+                Map< String, Object > geoMap = (Map< String, Object >) userMap.get("geo");
                 if (geoMap != null) {
                     profileUser.setLat((String) geoMap.get("lat"));
                     profileUser.setLng((String) geoMap.get("lng"));
                 }
 
 
-                Map<String, Object> companyMap = (Map<String, Object>) userMap.get("company");
+                Map< String, Object > companyMap = (Map< String, Object >) userMap.get("company");
                 if (companyMap != null) {
                     profileUser.setCompanyName((String) companyMap.get("name"));
                     profileUser.setCompanyCatchPhrase((String) companyMap.get("catchPhrase"));
